bd271eaf021d22492b70ea8a5cc74ce13b0d0597
morikawa_m <19ef092@ms.dendai.ac.jp>
2023-01-18 13:10:11 +0900

    実験15

--- dataset/mnist.py
+++ dataset/mnist.py
@@ -5,8 +5,7 @@ from torchvision import transforms
 
 def get_train_dataloader(root: str, batch_size: int=64):
     transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.RandomRotation(degrees=90),
+        transforms.ToTensor()
     ])
 
     train_dataset = MNIST(
@@ -26,8 +25,7 @@ def get_train_dataloader(root: str, batch_size: int=64):
 
 def get_test_dataloader(root: str, batch_size: int=64):
     transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.RandomRotation(degrees=90),
+        transforms.ToTensor()
     ])
 
     test_dataset = MNIST(
